9ac5e3acb98002b121a0f69d927310faa1755916
Kenneth Centurion <kcenturion@yahoo.com>
2016-03-16 11:44:22 -0500

    det

--- raspatron-web.sh
+++ raspatron-web.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 #
-# raspatron.sh
+# raspatron-web.sh
 #
-# chmod +x raspatron.sh
+# chmod +x raspatron-web.sh
 #
-
-#start the video stream
-echo "Starting video stream..."
-if pgrep mjpg_streamer > /dev/null
-then
-  echo "mjpg_streamer already running"
-else
-  LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 15 -q 50 -x 980 -y 768" -o "output_http.so -p 8171 -w /opt/mjpg-streamer/www -c $
-  echo "mjpg_streamer started"
-fi
 
 echo "Starting raspatron-web..."
 cd /usr/local/src/raspatron-web
